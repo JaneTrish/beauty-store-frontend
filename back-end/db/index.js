b9c47b5bb6e2f05f1dc05a31026b46915bdb8f8b
Jane <82611639+JaneTrish@users.noreply.github.com>
2022-11-08 22:46:53 +0200

    Update index.js

--- back-end/db/index.js
+++ back-end/db/index.js
@@ -1,6 +1,5 @@
 const { Pool, Client } = require('pg');
-const connectionString =
-  'postgres://daersurxxnmcex:e0da560f0026e165dc1c47afb019575a2194adf0e6d58c29d367b4e7b577d649@ec2-3-231-254-204.compute-1.amazonaws.com:5432/d4i8p44fnsafk';
+const connectionString = //connectionString;
 const pool = new Pool({
   connectionString,
   ssl: { rejectUnauthorized: false },
